e91d20ced48a8eb198d2be66d33efd2e06388196
xyfruux <victor.stampe2010@gmail.com>
2025-05-16 14:39:47 +0200

    Create Home Router

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import About from "./pages/About.jsx";
+import Home from "./pages/Home.jsx";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
-  { path: "/", element: <App /> },
+  { path: "/", element: <Home /> },
   { path: "/about", element: <About /> },
 ]);
 
