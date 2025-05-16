df296408d68d3f07a4322b523c4c524e22216392
xyfruux <victor.stampe2010@gmail.com>
2025-05-16 14:31:52 +0200

    Create Footer Component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+function Footer() {
+  return (
+    <footer>
+      <section>
+        <h3>Contact</h3>
+        <a href="mailto:victor.stampe2010@gmail.com">
+          victor.stampe2010@gmail.com
+        </a>
+      </section>
+      <section>
+        <h3>About</h3>
+        <a href="/about">About</a>
+      </section>
+    </footer>
+  );
+}
+
+export default Footer;
