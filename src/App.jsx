4669116dc8481f09deb34280ef808243bb7ca1ff
xyfruux <victor.stampe2010@gmail.com>
2025-05-16 07:47:51 +0200

    Create Card Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
+import Card from "./Card.jsx";
 
 function App() {
   return (
     <>
       <Header />
+      <Card />
       <Footer />
     </>
   );
---
+++ src/Card.jsx
@@ -0,0 +1,11 @@
+function Card() {
+  return (
+    <div className="card">
+      <img src="" alt="Card Logos" />
+      <h2>Card</h2>
+      <p>Description</p>
+    </div>
+  );
+}
+
+export default Card;
