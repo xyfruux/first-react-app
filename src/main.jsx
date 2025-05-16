12077e1cb1953f77bb289071132df2721ad47eed
xyfruux <victor.stampe2010@gmail.com>
2025-05-16 14:37:56 +0200

    Create About Router & Fix About Export

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import About from "./pages/About.jsx";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter([{ path: "/", element: <App /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/about", element: <About /> },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
